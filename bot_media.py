d54192a54caab98d82d00faaaf202ca5e85f3753
shufflek1ng <154771651+shufflek1ng@users.noreply.github.com>
2025-09-24 06:05:52 +0100

    Update bot_media.py

--- bot_media.py
+++ bot_media.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import yt_dlp
 import asyncio
 import re
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -192,4 +193,4 @@ async def stop(ctx):
     else:
         await ctx.send("⚠️ Nothing is playing.")
 
-bot.run("YOUR_DISCORD_BOT_TOKEN")
+bot.run(os.environ['DISCORD_BOT_TOKEN'])
